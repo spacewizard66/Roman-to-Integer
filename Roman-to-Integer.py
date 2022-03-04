fb3113dfc4b18792d850ee05aeb591a66d181f8e
Mason Galat <70931763+spacewizard66@users.noreply.github.com>
2022-03-04 01:57:43 -0500

    Add files via upload

---
+++ Roman-to-Integer.py
@@ -0,0 +1,38 @@
+# ACCEPTED !!!
+#I can be placed before V (5) and X (10) to make 4 and 9. 
+#X can be placed before L (50) and C (100) to make 40 and 90. 
+#C can be placed before D (500) and M (1000) to make 400 and 900.
+
+
+
+
+def roman_to_integer():
+    '''A Function to convert Romanumerals to Integers'''
+
+    #Romanumeral Key
+    roman_symbols = {
+        'I':1,
+        'V':5,
+        'X':10,
+        'L':50,
+        'C':100,
+        'D':500,
+        'M':1000
+    }
+
+    roman = input('') #Desired Input
+    
+    x = 0
+    last_digit = roman[-1] #Remembers Last Romanumeral Digit
+
+    #Reverses Romanumerals
+    for i in roman[::-1]:
+        roman_integer = roman_symbols[i] #Assigns the int of the Roman
+        if roman_symbols[last_digit] > roman_integer: #Checks if subtraction is needed
+            x -= roman_integer
+            continue
+        last_digit = i
+        x += roman_integer #Adds single Roman Integer to Total
+    print(x)
+
+roman_to_integer()
